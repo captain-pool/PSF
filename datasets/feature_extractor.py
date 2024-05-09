f83c08b3f326cbd7796a5e719aa942954674671f
Adrish Dey <rickdey1998@gmail.com>
2024-05-08 20:45:10 -0400

    rendering bug fix

--- datasets/feature_extractor.py
+++ datasets/feature_extractor.py
@@ -117,20 +117,29 @@ class ImageFeatureExtractor:
                 computed_result = self.model(self.transforms(inputs))
         return computed_result
 
-    def features(self, img_paths):
-        result = torch.empty(len(img_paths), self._ncomponents, dtype=torch.float32)
+    def features(self, img_paths=None, pil_imgs=None):
+        
+
+        assert img_paths is None or pil_imgs is None # We gotta do XOR
+
+        cached = bool(img_paths)
+
+        nimgs = len(img_paths) if img_paths is not None else len(pil_imgs)
+
+        result = torch.empty(nimgs, self._ncomponents, dtype=torch.float32)
 
         computed_data = []
         computed_indices = []
         cached_indices = []
         cached_data = []
 
-        for i, img_path in enumerate(img_paths):
-            if img_path.name not in self._frames_data:
-                img = PIL.Image.open(img_path).convert("RGB")
-                inputs = self.transforms(img)
-                computed_indices.append(i)
-                computed_data.append(inputs[None, ...])
+        for i in range(nimgs):
+            if cached and img_paths[i].name not in self._frames_data:
+                img = PIL.Image.open(img_paths[i]).convert("RGB")
+            elif pil_imgs is not None:
+              inputs = self.transforms(pil_imgs[i])
+              computed_indices.append(i)
+              computed_data.append(inputs[None, ...])
             else:
                 cached_indices.append(i)
                 cached_data.append(self._frames_data[img_path.name])
@@ -143,8 +152,10 @@ class ImageFeatureExtractor:
             #                    self._reduce.fit_transform(computed_result.detach().cpu().numpy())
             #                ).to(torch.float32)
             result[computed_indices] = computed_result
-            for idx in computed_indices:
-                self._frames_data[img_paths[idx].name] = result[idx]
+
+            if cached:
+              for idx in computed_indices:
+                  self._frames_data[img_paths[idx].name] = result[idx]
 
         if len(cached_indices) > 0:
             result[cached_indices] = torch.vstack(cached_data).to(result.device)
@@ -184,9 +195,10 @@ if __name__ == "__main__":
         except:
           continue
 
-        img_tensor = np.zeros((len(eyes), 224, 224, 3)).astype(np.float32)
+        imgs = [] 
         for i in range(len(eyes)):
-            img_tensor[i] = renderer.render_cloud(cloud, eye=eyes[i]).squeeze()
+            pixels = renderer.render_cloud(cloud, eye=eyes[i]).squeeze()
+            imgs.append(PIL.fromarray(pixels.astype(np.uint8)))
 
-        feats = extractor.features_img(img_tensor).numpy()
+        feats = extractor.features(pil_imgs=imgs).numpy()
         np.save(str(feat_path), feats)
